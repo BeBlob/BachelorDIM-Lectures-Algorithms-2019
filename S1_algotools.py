944c6302eea791c0b3d2b390b5c3091e5f0a8228
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2019-09-26 15:56:15 +0200

    refactor: add a function to calculate the average of a table above 0

--- S1_algotools.py
+++ S1_algotools.py
@@ -1,13 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Sep 26 14:18:50 2019
-
+@date: 26/09/2019 - 15h
 @author: BeBlob
 """
 
-print('My first python3 trials')
+"""Documentation for average_above_zero
+@param table:list
+get the average of a table above zero
+"""
+def average_above_zero(table):
+    sum = 0
+    cmpt_elem = 0
+    nb_elem = len(table)
 
-myVariable = 0
+    for cmpt in range(nb_elem) :
+        if table[cmpt] > 0 :
+            sum = sum + table[cmpt]
+            cmpt_elem = cmpt_elem + 1
+    moy = sum/cmpt_elem
+    return moy
 
-print('MyVariable = ', myVariable)
-
+print('La moyenne est : {moy}'.format(moy = average_above_zero([1,2,3,4,5])))
